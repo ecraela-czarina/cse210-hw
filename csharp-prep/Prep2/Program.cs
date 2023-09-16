c5bb4dd0b38ce482b900fdd62273e0303a61fb97
ecraela-czarina <avila.czarina10@gmail.com>
2023-09-16 18:31:43 +0800

    C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int x = int.Parse(grade);
+
+        string letter = "";
+
+        if (x >= 90)
+        {
+
+            letter = "A";
+        }
+
+        else if (x >= 80)
+
+        {
+            letter = "B";
+        }
+
+        else if (x >= 70)
+
+        {
+            letter = "C";
+        }
+
+        else if (x >= 60)
+
+        {
+            letter = "D";
+        }
+
+        else if (x < 60)
+
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations. You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time.");
+        }
     }
 }

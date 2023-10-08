d76c5124257c7aa74e45dbc07e9e2d63a776643a
ecraela-czarina <avila.czarina10@gmail.com>
2023-10-09 00:14:06 +0800

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment newMathAssign = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19" );
+        Console.WriteLine(newMathAssign.GetSummary());
+        Console.WriteLine(newMathAssign.GetHomeworkList());
+
+        WritingAssignment newWritingAssign = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters" );
+        Console.WriteLine(newWritingAssign.GetSummary());
+        Console.WriteLine(newWritingAssign.GetWritingInfo());
     }
 }
